a1409bc1b47fe02c16e045b192c17541b5158113
javad-c <134827630+javad-c@users.noreply.github.com>
2023-06-12 13:09:06 +0330

    end of design the first page
    end of design the 1st page

--- jamin-gram/main.cpp
+++ jamin-gram/main.cpp
@@ -6,6 +6,8 @@ int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     first_page w;
+    w.setWindowTitle("jamin gram");
+    w.setWindowFlags(Qt::Dialog);
     w.show();
     return a.exec();
 }
